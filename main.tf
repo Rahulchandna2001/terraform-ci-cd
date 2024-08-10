80ef49982ec446b2605d102bbd8d473bac8ad000
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-08-09 20:53:04 -0400

    committed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu1" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   cpu_options {
