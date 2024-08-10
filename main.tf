f2c6a5afc82eeeb3e050992c7c75ec1b24670046
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-08-10 00:02:50 -0400

    committed last

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "ubuntu1" {
   ami           = "ami-04a81a99f5ec58529"
   instance_type = "t3.micro"
   cpu_options {
-    core_count       = var.core_count
-    threads_per_core = var.thread_count
+    core_count       = 1
+    threads_per_core = 2
   }
 
   tags = {
