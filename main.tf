2d1de38329ba2e686bc9b87a99272cd800c75270
Rahulchandna2001 <95170585+Rahulchandna2001@users.noreply.github.com>
2024-08-10 00:08:42 -0400

    committed last

--- main.tf
+++ main.tf
@@ -2,15 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "ubuntu1" {
-  ami           = "ami-04a81a99f5ec58529"
-  instance_type = "t3.micro"
-  cpu_options {
-    core_count       = 1
-    threads_per_core = 2
-  }
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "rahul"
+    Name = "ExampleInstance"
   }
 }
+
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+  default     = "t2.micro"  # You can change this default value to whatever is appropriate
+}
+
